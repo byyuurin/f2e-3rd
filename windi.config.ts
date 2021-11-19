f789f16824993f3c8324e08e7726bfc136529d91
yuurin <byyuurin@gmail.com>
2021-11-19 13:27:16 +0800

    啟用 windicss 的 Attributify Mode

--- windi.config.ts
+++ windi.config.ts
@@ -3,6 +3,7 @@ import LineClampPlugin from 'windicss/plugin/line-clamp';
 
 export default defineConfig({
   darkMode: 'class',
+  attributify: true,
   theme: {},
   plugins: [LineClampPlugin]
 });
