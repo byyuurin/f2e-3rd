10fabe5a1a42dc67085842c082519576c944f04e
yuurin <byyuurin@gmail.com>
2021-11-22 15:12:44 +0800

    UI 共享設定獨立單檔管理

---
+++ src/components/ui/_shared.ts
@@ -0,0 +1,13 @@
+export type InputSize = 'small' | 'medium' | 'large';
+
+export const inputHeight = {
+  small: 'h-10',
+  medium: 'h-12',
+  large: 'h-14'
+};
+
+export const inputFontSize = {
+  small: 'text-sm',
+  medium: 'text-base',
+  large: 'text-2xl'
+};
