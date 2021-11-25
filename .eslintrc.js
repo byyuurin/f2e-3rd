124257e7fa392949fa46b839239add350c5505c8
yuurin <byyuurin@gmail.com>
2021-11-25 13:01:52 +0800

    調整 eslint 規則

--- .eslintrc.js
+++ .eslintrc.js
@@ -28,6 +28,7 @@ module.exports = {
     'no-var': 'error',
 
     // ts
+    '@typescript-eslint/no-non-null-assertion': 'off',
     '@typescript-eslint/no-explicit-any': 'off',
     '@typescript-eslint/explicit-module-boundary-types': 'off',
     'no-undef': 'off',
